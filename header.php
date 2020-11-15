bb19662c1d3de1c2e7096ff2889b60763f07dfb0
Friedrich Eibl <friedrich.eibl@hu-berlin.de>
2020-11-15 22:45:27 +0100

    Login completed

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <link>
+        <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
     </head>
 
     <body>
@@ -15,8 +15,8 @@
     <nav>
         <div class="wrapper">
             <?php
-                if (isset($_SESSION["usersID"])) {
-                    echo "<li><a>You are logged in!</a></li>";
+                if (isset($_SESSION["username"])) {
+                    echo "<li><a>You are logged in as " . $_SESSION["username"] . "!</a></li>";
                     echo "<li><a href='includes/logout.inc.php'>Logout</a></li>";
                 } else {
                     echo "<li><a>You need to login!</a></li>";
